f78b76fd044601fda640ee8a46da64083b7d898c
PATATE12 <jesus2099@yahoo.com>
2011-08-02 18:30:00 +0200

    loads hidden links from Relationships tab + more generated links

--- mb_hidden_links.user.js
+++ mb_hidden_links.user.js
@@ -1,7 +1,8 @@
 // ==UserScript==
-// @name           mb: Display lastfm links
-// @description    Adds a link to artsit's last.fm page in musicbrainz sidebar
-// @version        2011-08-02_1400
+// @name           mb: Display hidden and generated links in sidebar (lastfm, searches, etc.)
+// @description    Hidden links include fanpage, social network, etc. (NO duplicates) Generated links (configurable) includes Google, auto last.fm, Discogs and LyricWiki searches, etc.
+// @version        2011-08-02_1828
+// @history        2011-08-02_1828 loads hidden links from Relationships tab + more generated links
 // @history        2011-08-02_1400 fix URL encoding for non-Opera
 // @history        2011-08-02_1246 link to lastfm artists
 // @author         Tristan DANIEL (jesus2099)
@@ -10,24 +11,123 @@
 // @namespace      http://userscripts.org/scripts/show/108889
 
 // @include        http://*musicbrainz.org/artist/*
-
+// @exclude        http://*musicbrainz.org/artist/*/edit
 // ==/UserScript==
 
 (function () {
 /*todo:releases(-groups), recordings and works*/
-var lastfmartist = "http://last.fm/%a%";
+
+/*settings*/
+var artist_autolinks = {
+	"Lastfm (mbid)": "http://last.fm/mbid/%artist-id%",
+	"Lastfm (name)": "http://last.fm/music/%artist-name%",
+	"Discogs search": "http://www.discogs.com/search?q=%artist-name%&type=artists",
+	"LyricWiki": "http://lyrics.wikia.com/%artist-name%",
+	"Google (strict)": "http://google.com/search?q=%2B%22%artist-name%%22",
+	"Google (normal)": "http://google.com/search?q=%artist-name%",
+};
+/*end of settings*/
+
 var sidebar = document.getElementById("sidebar");
+var arelsws = "/ws/2/artist/%artist-id%?inc=url-rels";
+var existingLinks;
+
 if (sidebar) {
-	var extlinks = sidebar.getElementsByClassName("external_links");
-	if (extlinks.length>0) { extlinks = extlinks[0]; }
-	else { extlinks = sidebar; }
-	var artistname = document.getElementsByTagName("h1")[0].getElementsByTagName("a")[0].firstChild.nodeValue.trim();
-	var li = document.createElement("li");
-	var a = document.createElement("a");
-	a.setAttribute("href", lastfmartist.replace(/%a%/, encodeURIComponent(artistname)));
-	a.appendChild(document.createTextNode("Last.fm"));
-	li.appendChild(a);
-	extlinks.appendChild(li);
+	var artistid = self.location.href.match(/musicbrainz.org\/artist\/([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}).*/i);
+	if (artistid) {
+		artistid = artistid[1];
+		var artistname = document.getElementsByTagName("h1")[0].getElementsByTagName("a")[0].firstChild.nodeValue.trim();
+		var extlinks = sidebar.getElementsByClassName("external_links");
+		if (extlinks && extlinks.length>0) {
+			extlinks = extlinks[0];
+			loading(true);
+			/*attached missing links*/
+			var xhr = new XMLHttpRequest();
+			xhr.onreadystatechange = function(e) {
+				if (xhr.readyState == 4 && xhr.status == 200) {
+					var res = xhr.responseXML;
+					var urls = res.evaluate("//relation-list[@target-type='url']/relation", res, null, XPathResult.ANY_TYPE, null);
+					var haslinks = false;
+					while (url = urls.iterateNext()) {
+						var target = res.evaluate("./target", url, null, XPathResult.ANY_TYPE, null);
+						var turl = target.iterateNext();
+						if (turl) {
+							if (!haslinks) {
+								haslinks = true;
+								addExternalLink("Hidden links");
+							}
+							addExternalLink(url.getAttribute("type"), turl.textContent);
+						}
+					}
+					/*artist_autolinks*/
+					haslinks = false;
+					for (link in artist_autolinks) {
+						if (!haslinks) {
+							haslinks = true;
+							addExternalLink("Generated links");
+						}
+						addExternalLink(link, artist_autolinks[link].replace(/%artist-id%/, artistid).replace(/%artist-name%/, encodeURIComponent(artistname)));
+					}
+					loading(false);
+				}
+			};
+			xhr.open("GET", arelsws.replace(/%artist-id%/, artistid), true);
+			xhr.send(null);
+		}
+	}/*artist*/
+}
+
+function addExternalLink(text, url) {
+	var lis = extlinks.getElementsByTagName("li");
+	if (!existingLinks) {
+		existingLinks = [];
+		for (ilis=0; ilis<lis.length; ilis++) {
+			var lisas = lis[ilis].getElementsByTagName("a");
+			if (lisas.length>0) { existingLinks.push(lisas[0].getAttribute("href").trim()); }
+		}
+	}
+	if (url) {
+		if (existingLinks.indexOf(url.trim()) == -1) {
+			existingLinks.push(url.trim());
+			var li = document.createElement("li");
+			li.className = text;
+			var a = document.createElement("a");
+			a.setAttribute("href", url);
+			a.appendChild(document.createTextNode(text));
+			li.appendChild(a);
+			extlinks.appendChild(li);
+		}
+	}
+	else {
+		var li = document.createElement("li");
+		li.style.fontWeight = "bold";
+		li.appendChild(document.createTextNode(text));
+		extlinks.appendChild(li);
+	}
+}
+
+function loading(on) {
+	var ldng = document.getElementById("jesus2099loading108889");
+	if (on) {
+		if (!ldng) {
+			var li = document.createElement("li");
+			li.setAttribute("id", "jesus2099loading108889");
+			li.style.background = "#ff6";
+			li.appendChild(document.createTextNode("loading\u2026"));
+			var lis = extlinks.getElementsByTagName("li");
+			if (lis.length == 1 && lis[0].lastChild.nodeValue.indexOf("has no URL relationships.") != -1) {
+				extlinks.replaceChild(li, lis[0]);
+			}
+			else {
+				extlinks.appendChild(li);
+			}
+		}
+	}
+	else {
+		if (ldng) {
+			ldng.parentNode.removeChild(ldng);
+		}
+	}
 }
 
 })();
